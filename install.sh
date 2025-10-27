792ef888ee3f24466c98c47e09a8e69b05bcaaed
gg582 <gzblues61@daum.net>
2025-10-27 11:02:12 +0900

    Add installer script

---
+++ install.sh
@@ -0,0 +1,5 @@
+make
+sudo cp achim_alarm /usr/local/bin
+sudo cp shortcuts/policy/com.gg582.achim-alarm.policy /usr/share/polkit-1/actions/
+sudo cp shortcuts/achim.desktop /usr/share/applications
+
